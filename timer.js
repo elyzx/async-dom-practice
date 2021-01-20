f7a2412a9d3421a5313c406db506c34d6218ebdf
Manish Poduval <manishpoduval@hotmail.com>
2021-01-20 12:18:22 +0530

    async dom practice

---
+++ timer.js
@@ -0,0 +1,13 @@
+// add properties and methods for your timer here
+class Timer {
+    //STEP 0: Add necessary properties for your timer
+
+
+    // STEP1: Create a start method to start the timer
+
+
+
+    // STEP2: Create a stop method to stop the timer
+
+
+}
