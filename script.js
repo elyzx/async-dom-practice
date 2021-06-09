8b9dde989e59a45de1f6112a14a5794a63ee8490
Ellen Children <ellenchildren@icloud.com>
2021-06-09 16:00:08 +0200

    completed task

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ let startBtn = document.querySelector('#start')
 let stopBtn = document.querySelector('#stop')
 let timeDOM = document.querySelector('#timer')
 
-
 // STEP 3: Create an object from the timer class
 
+let timer = new Timer()
+
 function updateTime(){
-   // update the time DOM here
-   // console.log('Here')
-    
+    timeDOM.innerText = timer.time
 }
 
 
 startBtn.addEventListener('click', () => {
     // Call the timer start method
+    timer.start(updateTime)
 })
 
 
 stopBtn.addEventListener('click', () => {
     // Call the timer stop method
+    timer.stop()
 })
